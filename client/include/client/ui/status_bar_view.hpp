f398a7ee72e41749beab9b77fce6f4ff4020b0bf
RomanSnitko <romsnitko@gmail.com>
2026-05-19 20:59:35 +0300

    client: drop legacy status bar view

--- client/include/client/ui/status_bar_view.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace web_htop::client::ui {
-
-[[nodiscard]] std::string RenderStatusBar(bool connected, const std::string& message);
-
-}  // namespace web_htop::client::ui
