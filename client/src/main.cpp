66a9003a45748fc990082eb5b38fcbbf66eecda1
RomanSnitko <romsnitko@gmail.com>
2026-05-22 18:10:08 +0300

    client: implement main

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,42 +1,50 @@
 #include "client/app/client_app.hpp"
-
-#include <csignal>
-#include <cstdlib>
+#include <charconv>
 #include <iostream>
-#include <string>
-
+#include <string_view>
 namespace {
-
-web_htop::client::ClientApp* g_client = nullptr;
-
-void HandleSignal(int sig) {
-    if ((sig == SIGINT || sig == SIGTERM) && g_client != nullptr) {
-        g_client->stop();
-    }
+unsigned Port(std::string_view s) {
+    unsigned n{};
+    auto [p, ec] = std::from_chars(s.data(), s.data() + s.size(), n);
+    if (ec != std::errc{} || p != s.data() + s.size() || n == 0 || n > 65535)
+        throw std::invalid_argument("port must be 1..65535");
+    return n;
 }
-
-}  // namespace
-
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <host> <streaming_port> [http_port]" << std::endl;
+} // namespace
+int main(int argc, char** argv) {
+    try {
+        web_htop::client::ClientOptions options;
+        unsigned positional = 0;
+        for (int i = 1; i < argc; ++i) {
+            std::string_view arg = argv[i];
+            if (arg == "--help") {
+                std::cout
+                    << "web_htop_client [host] [stream_port] [http_port] [--once] [--record FILE]\n"
+                       "web_htop_client --replay FILE [--once]\n";
+                return 0;
+            }
+            if (arg == "--once")
+                options.once = true;
+            else if (arg == "--record" || arg == "--replay") {
+                if (i + 1 == argc)
+                    throw std::invalid_argument("missing file path");
+                (arg == "--record" ? options.record_path : options.replay_path) = argv[++i];
+            } else if (arg.starts_with('-'))
+                throw std::invalid_argument("unknown option");
+            else if (positional++ == 0)
+                options.host = arg;
+            else if (positional == 2)
+                options.stream_port = Port(arg);
+            else if (positional == 3)
+                options.http_port = Port(arg);
+            else
+                throw std::invalid_argument("too many arguments");
+        }
+        if (!options.record_path.empty() && !options.replay_path.empty())
+            throw std::invalid_argument("record and replay are mutually exclusive");
+        return web_htop::client::ClientApp(std::move(options)).Run();
+    } catch (std::exception const& e) {
+        std::cerr << "web_htop_client: " << e.what() << '\n';
         return 1;
     }
-
-    const std::string host = argv[1];
-    const int streaming_port = std::atoi(argv[2]);
-    const int http_port = (argc >= 4) ? std::atoi(argv[3]) : 8080;
-
-    if (streaming_port <= 0 || http_port <= 0) {
-        std::cerr << "Invalid port values." << std::endl;
-        return 1;
-    }
-
-    std::signal(SIGINT, HandleSignal);
-    std::signal(SIGTERM, HandleSignal);
-    std::signal(SIGPIPE, SIG_IGN);
-
-    web_htop::client::ClientApp app(host, streaming_port, http_port);
-    g_client = &app;
-    return app.run();
 }
