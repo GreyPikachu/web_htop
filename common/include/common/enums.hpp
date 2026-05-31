d20b968be2007be8269786e1bc68f07d28e4c0a4
RomanSnitko <romsnitko@gmail.com>
2026-06-01 02:27:25 +0300

    common: implement enums

--- common/include/common/enums.hpp
+++ common/include/common/enums.hpp
@@ -1,7 +1,7 @@
 /**
  * @file common/enums.hpp
  *
- * @author Maksim Vashkevich
+ * @author Roman Snitko
  * @date 2026-04-03
  *
  * @brief Common enumerations for web_htop project
@@ -18,17 +18,17 @@ namespace web_htop {
  * @brief Process state enumeration based on /proc/[pid]/stat field 3.
  */
 enum class ProcessState : char {
-    RUNNING      = 'R', // Running
-    SLEEPING     = 'S', // Sleeping in interruptible wait
-    DISK_SLEEP   = 'D', // Waiting in uninterruptible disk sleep
-    ZOMBIE       = 'Z', // Zombie
-    STOPPED      = 'T', // Stopped (on a signal)
+    RUNNING = 'R',      // Running
+    SLEEPING = 'S',     // Sleeping in interruptible wait
+    DISK_SLEEP = 'D',   // Waiting in uninterruptible disk sleep
+    ZOMBIE = 'Z',       // Zombie
+    STOPPED = 'T',      // Stopped (on a signal)
     TRACING_STOP = 't', // Tracing stop
-    DEAD         = 'X', // Dead
-    WAKING       = 'W', // Waking
-    PARKED       = 'P', // Parked
-    IDLE         = 'I', // Idle (Linux 5.6+)
-    UNKNOWN      = '?'  // Unknown state
+    DEAD = 'X',         // Dead
+    WAKING = 'W',       // Waking
+    PARKED = 'P',       // Parked
+    IDLE = 'I',         // Idle (Linux 5.6+)
+    UNKNOWN = '?'       // Unknown state
 };
 
 } // namespace web_htop
