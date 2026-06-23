a6ea7f13e90a32ea358e65ccd2f94d8a56d89504
RomanSnitko <romsnitko@gmail.com>
2026-06-23 18:13:36 +0300

    common: update memory metrics

--- common/src/common/models/memory_metrics.cpp
+++ common/src/common/models/memory_metrics.cpp
@@ -1,7 +1,7 @@
 /**
  * @file common/models/memory_metrics.cpp
  *
- * @author Maksim Vashkevich
+ * @author Roman Snitko
  * @date 2026-04-08
  *
  * @brief Memory metrics model serialization implementation.
@@ -11,23 +11,23 @@
 #include "common/models/memory_metrics.hpp"
 
 namespace web_htop::models {
- 
+
 json::utils::JSONValue MemoryMetrics::ToJson() const {
     json::utils::JSONObject object{};
     object.reserve(5);
- 
+
     object.emplace_back("timestamp", json::utils::JSONValue(static_cast<std::uint64_t>(timestamp)));
     object.emplace_back("total_bytes", json::utils::JSONValue(total_bytes));
     object.emplace_back("available_bytes", json::utils::JSONValue(available_bytes));
     object.emplace_back("used_bytes", json::utils::JSONValue(used_bytes));
     object.emplace_back("used_percent", json::utils::JSONValue(used_percent));
-     
+
     return json::utils::JSONValue(std::move(object));
 }
- 
-MemoryMetrics MemoryMetrics::FromJson(json::utils::JSONValue const & value) {
+
+MemoryMetrics MemoryMetrics::FromJson(json::utils::JSONValue const& value) {
     MemoryMetrics metrics{};
- 
+
     if (auto ts = value["timestamp"]) {
         if (auto parsed = ts->get().AsUInt64()) {
             metrics.timestamp = *parsed;
@@ -53,8 +53,8 @@ MemoryMetrics MemoryMetrics::FromJson(json::utils::JSONValue const & value) {
             metrics.used_percent = *parsed;
         }
     }
- 
+
     return metrics;
 }
- 
-} // namespace web_htop::models 
+
+} // namespace web_htop::models
