a029eafcbcf88818a000135a3c5dffd365f50c29
RomanSnitko <romsnitko@gmail.com>
2026-07-28 09:59:25 +0300

    server: wire up http request

---
+++ server/include/server/http/http_request.hpp
@@ -0,0 +1,15 @@
+/** @file server/http/http_request.hpp
+ *  @brief Strict GET-only HTTP/1.x subset; one request per connection.
+ */
+#pragma once
+#include <string>
+#include <string_view>
+namespace web_htop::server {
+struct HttpRequest {
+    unsigned status{200};
+    std::string target;
+};
+[[nodiscard]] HttpRequest ParseHttpRequest(std::string_view text);
+[[nodiscard]] std::string HttpResponse(unsigned status, std::string_view body,
+                                       std::string_view type = "application/json");
+} // namespace web_htop::server
