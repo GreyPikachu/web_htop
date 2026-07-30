80f96c7e2c01e87797d81a4d93ade64a409b81d8
RomanSnitko <romsnitko@gmail.com>
2026-07-30 22:31:20 +0300

    server: wire up linux source

--- server/include/server/system/linux_source.hpp
+++ server/include/server/system/linux_source.hpp
@@ -1,2 +1,38 @@
+/**
+ * @file server/system/linux_source.hpp
+ * @brief Linux reads behind an injectable boundary; parsers never open files.
+ */
 #pragma once
-// TODO: fill in the interface.
+#include "server/config/server_config.hpp"
+#include <cstdint>
+#include <string>
+#include <sys/statvfs.h>
+#include <vector>
+
+namespace web_htop::server::system {
+struct FileResult {
+    std::string text;
+    int error{};
+    [[nodiscard]] explicit operator bool() const noexcept {
+        return error == 0;
+    }
+};
+struct PidList {
+    std::vector<int> pids;
+    int error{};
+};
+class LinuxSource {
+  public:
+    explicit LinuxSource(ServerConfig config) : config_(std::move(config)) {}
+    virtual ~LinuxSource() = default;
+    [[nodiscard]] virtual FileResult Proc(std::string const& relative) const;
+    [[nodiscard]] virtual FileResult Sys(std::string const& relative) const;
+    [[nodiscard]] virtual FileResult Cgroup(std::string const& relative) const;
+    [[nodiscard]] virtual PidList Pids() const;
+    [[nodiscard]] virtual int Filesystem(struct statvfs& result) const;
+    [[nodiscard]] virtual std::string CgroupIdentity() const;
+
+  private:
+    ServerConfig config_;
+};
+} // namespace web_htop::server::system
